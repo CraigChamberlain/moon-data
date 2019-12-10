91e48553e297f68ff48ce0790abe976b00555cb2
Craig <craig@DebianDesktop.home>
2019-12-10 13:08:02 +0000

    Add a detailed calendar

--- tools/makeCalendar.ps1
+++ tools/makeCalendar.ps1
@@ -83,3 +83,58 @@ function CreateLunarSolarCalendarSingleMonths(){
 
     }
 }
+
+function AttachPhases($months){
+    $Year = $months[0].Date.Year
+    $PhaseData = Get-ChildItem "./api/moon-phase-data/$Year" -Recurse -File | Get-Content | ConvertFrom-Json
+ 
+    for ($i = 0; $i -lt $months.Count; $i++) {
+        if ($i -ne $months.Count - 1){
+            $Phases =  
+                $PhaseData | Where-Object {$_.Date -ge $months[$i].Date -and $_.Date -lt $months[$i+1].Date}
+            if ($i -eq 0 -and $PhaseData[0].Date -ne  $months[$i+1].Date ){
+                
+                $Phases =  
+                         @(New-Object PSObject -Property @{ Date = $months[$i].Date; Phase = -1}) + $Phases
+                }
+        }
+        else {
+            $Phases =  
+                $PhaseData | Where-Object {$_.Date -ge $months[$i].Date}
+        }
+    
+        $props = 
+            @{
+                Month = $i; 
+                Phases = $Phases; 
+                Days = $months[$i].Days; 
+                Date = $months[$i].Date;
+            }
+
+        New-Object PSObject -Property $props
+
+    }
+    
+
+} 
+
+function CreateLunarSolarCalendarDetailed(){
+    $years = Get-ChildItem './api/lunar-solar-calendar/' -Recurse -File -Depth 1
+    $years | 
+    ForEach-Object {
+        $Months = readJson -file $_;  
+        $file = ($_.FullName -replace "lunar-solar-calendar", "lunar-solar-calendar-detailed" );
+        $Phases = AttachPhases -months $Months
+        mkdir  ($file -replace "index.json", "")
+        $Phases | ConvertTo-Json -Compress -Depth 3 | Out-File $file -NoNewline
+        $i = 0
+        foreach($Month in $Phases) {
+            $Name = $file -replace "index.json", "$i/index.json"
+            mkdir  ($Name -replace "index.json", "") 
+            $Month |
+            ConvertTo-Json -Compress | 
+            Out-File $Name -NoNewline
+            $i ++
+        } 
+    }
+}
